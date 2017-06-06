c70a9b0e22a60cc98bb554fd96a12076c8391e8e
Kresten P. Vester <kresten@vester.org>
2017-06-06 21:50:50 +0200

    Set correct content-type header

--- techtopia.php
+++ techtopia.php
@@ -1,4 +1,6 @@
 <?php
+    header('Content-Type: text/xml');
+
     $doc = new DOMDocument();
     $doc->load( 'http://tv.ida.dk/audiopodcast/channel/16944635' );
     foreach ($doc->getElementsByTagNameNS('http://search.yahoo.com/mrss/','content') as $element) {
